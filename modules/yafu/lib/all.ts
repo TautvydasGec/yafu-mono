db3d5644c9449674b1bb64810a9e80341a019ca5
Ludwig Magnusson <ludwig@mediatool.com>
2021-03-30 08:57:16 +0200

    partial migration

--- modules/yafu/lib/all.ts
+++ modules/yafu/lib/all.ts
@@ -5,6 +5,6 @@
  * @arg predicate {Function} The predicate function
  * @arg list {Array} The list of elements to check
  */
-export default function all (predicate, list) {
+export function all <A> (predicate: (a: A) => boolean, list: A[]): boolean {
   return list.length === 0 || (predicate(list[0]) === true && all(predicate, list.slice(1)))
 }
