167ee98d95301aebd3e767a23fc3cc994f060073
Ludwig Magnusson <ludwig@mediatool.com>
2021-04-05 11:08:48 +0200

    remove ts warnings

--- modules/yafu/lib/is-empty.ts
+++ modules/yafu/lib/is-empty.ts
@@ -3,6 +3,7 @@
  *
  * @arg list {Array} The list to check if it is empty or not.
  */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function isEmpty (list: any[]): boolean {
   return list.length === 0
 }
