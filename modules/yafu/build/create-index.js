0e36d5a39ab415d0346f1430ae4a803537071a2d
Ludwig Magnusson <ludwig@mediatool.com>
2021-04-23 13:28:58 +0200

    generate index file for types in yafu

--- modules/yafu/build/create-index.js
+++ modules/yafu/build/create-index.js
@@ -7,6 +7,14 @@ import {
   normalize,
 } from 'path'
 
+function appendJS (f) {
+  return `${f}.js`
+}
+
+function createImportExport (item) {
+  return `export * from './${item}'`
+}
+
 function createIndex (projectPath) {
   function getFileBaseName (fullName) {
     const ext = extname(fullName)
@@ -21,13 +29,12 @@ function createIndex (projectPath) {
     .map(getFileBaseName)
     .filter((s) => s.indexOf('_') !== 0)
 
-  const imports = jsFiles.map((item) => {
-    const pathName = `./${item}.js`
-    return `export * from '${pathName}'`
-  }).join('\n')
+  const jsImports = jsFiles.map((f) => createImportExport(appendJS(f))).join('\n')
+  const typeDefImports = jsFiles.map((f) => createImportExport(f)).join('\n')
 
   mkdirpSync('dist')
-  writeFileSync(getAbsolute('dist/index.js'), imports)
+  writeFileSync(getAbsolute('dist/index.js'), jsImports)
+  writeFileSync(getAbsolute('dist/index.d.ts'), typeDefImports)
 }
 
 createIndex(process.cwd())
